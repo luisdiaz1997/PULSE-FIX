0761a6aaafd756fa398afee1aee386878bac3f72
luisdiaz1997 <luisfcd2013@hotmail.com>
2020-07-09 02:57:14 -0700

    Added support for Ubuntu 20

--- fix-pulse.sh
+++ fix-pulse.sh
@@ -2,4 +2,5 @@ grep -rl $OSNAME=$(lsb_release -d |grep -o "Ubuntu") tmp | xargs sed -i 's+grep 
 grep -rlE '\s19|_19' tmp --exclude *tgz| xargs sed -i 's+_19+_20+g; s+\s19+ 20+g'
 dpkg-deb -b tmp pulse-fixed.deb
 sudo dpkg -i pulse-fixed.deb
+sudo apt-get install libenchant1c2a
 /usr/local/pulse/PulseClient_x86_64.sh install_dependency_packages
