4ac63aa906ec1a1db849689281dd831adf4e1767
luisdiaz1997 <luisfcd2013@hotmail.com>
2020-07-09 01:08:46 -0700

    added dependendencies install

--- fix-pulse.sh
+++ fix-pulse.sh
@@ -1,3 +1,4 @@
 grep -rl $OSNAME=$(lsb_release -d |grep -o "Ubuntu") tmp | xargs sed -i 's+grep -o "Ubuntu"+grep -o "Ubuntu\\|Pop"+g'
 dpkg-deb -b tmp pulse-fixed.deb
 sudo dpkg -i pulse-fixed.deb
+/usr/local/pulse/PulseClient_x86_64.sh install_dependency_packages
