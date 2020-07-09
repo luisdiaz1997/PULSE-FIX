1fa041413b17b599745b7b931c6138c6e392115b
luisdiaz1997 <luisfcd2013@hotmail.com>
2020-07-09 01:05:17 -0700

    added tmp

--- fix-pulse.sh
+++ fix-pulse.sh
@@ -1,8 +1,3 @@
-
-
-mkdir tmp
-dpkg-deb -R ps-pulse-ubuntu-debian.deb tmp
-grep -rl $OSNAME=$(lsb_release -d |grep -o "Ubuntu") tmp | xargs sed -i 's+grep -o "Ubuntu"+grep -o "Ubuntu\\|Zorin OS"+g'
+grep -rl $OSNAME=$(lsb_release -d |grep -o "Ubuntu") tmp | xargs sed -i 's+grep -o "Ubuntu"+grep -o "Ubuntu\\|Pop"+g'
 dpkg-deb -b tmp pulse-fixed.deb
-rm -rf tmp
-
+sudo dpkg -i pulse-fixed.deb
