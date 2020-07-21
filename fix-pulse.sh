aaec7977362bb62f5e31521fa3446711d4974de3
Luis Chumpitaz <luisfcd2013@hotmail.com>
2020-07-21 02:46:23 -0700

    Update fix-pulse.sh

--- fix-pulse.sh
+++ fix-pulse.sh
@@ -3,4 +3,5 @@ grep -rlE '\s19|_19' tmp --exclude *tgz| xargs sed -i 's+_19+_20+g; s+\s19+ 20+g
 dpkg-deb -b tmp pulse-fixed.deb
 sudo dpkg -i pulse-fixed.deb
 sudo apt-get install libenchant1c2a
+sudo apt-get install libcanberra-gtk-module
 /usr/local/pulse/PulseClient_x86_64.sh install_dependency_packages
